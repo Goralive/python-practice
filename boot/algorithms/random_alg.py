f3c0f24f609617bc0042149bddd9a5508a04b433
Igor Nosovskyi <nosovskyigor@gmail.com>
2024-07-06 21:51:11 +0200

    add section sort

--- boot/algorithms/random_alg.py
+++ boot/algorithms/random_alg.py
@@ -250,3 +250,13 @@ def partition(nums, low, high):
             i += 1
     nums[i], nums[high] = nums[high], nums[i]
     return i
+
+
+def selection_sort(nums):
+    for i in range(len(nums)):
+        smallest_idx = i
+        for j in range(i + 1, len(nums)):
+            if nums[j] < nums[smallest_idx]:
+                smallest_idx = j
+        nums[i], nums[smallest_idx] = nums[smallest_idx], nums[i]
+    return nums
